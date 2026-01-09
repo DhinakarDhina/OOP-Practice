2f611681b51ad0f7eeea537809a7c6f8afc4b843
Dhinakar.S <146498273+DhinakarDhina@users.noreply.github.com>
2026-01-09 20:51:37 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,63 +1,70 @@
-package EmployeeSalaryDetails;
+package InheritancePractice;
 
 import java.util.Scanner;
 
 public class Employee {
-    public String name;
-    protected int employeeId;
-    private double salary;
+    String name;
+    int id;
 
-    //constructor
-    Employee(String name, int employeeId, double salary){
+    Employee(String name, int id){
         this.name= name;
-        this.employeeId= employeeId;
-
-        if(salary<0){
-            System.out.println("Invalid salary");
-        }
-        else{
-            this.salary= salary;
-        }
+        this.id= id;
     }
-
-    //setting the salary of the employee
-    public void setSalary(double salary){
-        if(salary<0){
-            System.out.println("Invalid salary");
-            this.salary=0;
-        }
-        else{
-            this.salary= salary;
-        }
-    }
-
-    //Getting the salary of the employee
-    public double getSalary(){
-        return this.salary= salary;
-    }
-
-    //Displaying the employee details
-
-    public void displayEmployeeDetails(){
-        System.out.println("Name: "+name);
-        System.out.println("EmployeeId: "+employeeId);
-        System.out.println("Salary: "+salary);
-    }
+     public void displayDetails(){
+         System.out.println("Name: "+name);
+         System.out.println("Id: "+id);
+     }
 }
-class main{
-    public static void main(String []args){
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the name: ");
-        String name= in.nextLine();
-        System.out.println("Enter the employeeId: ");
-        int employeeId =in.nextInt();
-        System.out.println("Enter the salary: ");
-        double salary= in.nextDouble();
-        Employee e = new Employee(name, employeeId,salary);
-        e.setSalary(salary);
-        e.getSalary();
-        e.displayEmployeeDetails();
+
+class Manager extends Employee{
+    int team_size;
+
+    Manager(String name,int id, int team_size) {
+        super(name, id);
+        this.team_size = team_size;
+    }
+        public void displayDetails(){
+            super.displayDetails();
+            System.out.println("Team Size: "+team_size);
+
+        }
+}
+class Engineer extends Employee{
+     String specialization;
+
+     Engineer(String name, int id, String specialization){
+         super(name,id);
+         this.specialization=specialization;
+     }
+     public void displayDetails(){
+         super.displayDetails();
+         System.out.println("Employee's Specialization: "+specialization);
+     }
+}
+
+class Main{
+    public static void main(String[]args){
+        Scanner in= new Scanner(System.in);
+        System.out.println("Enter the Manager Name: ");
+        String managerName= in.nextLine();
+        System.out.println("Enter the Manager id: ");
+        int manager_id= in.nextInt();
+        System.out.println("Enter the Team size: ");
+        int team_size=in.nextInt();
+        in.nextLine();
+        System.out.println("Enter the Employee Name: ");
+        String EmployeeName= in.nextLine();
+        System.out.println("Enter the Employee id: ");
+        int employee_id= in.nextInt();
+        in.nextLine();
+        System.out.println("Enter the Specilization : ");
+        String specialization=in.nextLine();
+
+        Manager m= new Manager(managerName,manager_id,team_size);
+        m.displayDetails();
+
+        Engineer e= new Engineer(EmployeeName,employee_id,specialization);
+        e.displayDetails();
 
     }
-
 }
